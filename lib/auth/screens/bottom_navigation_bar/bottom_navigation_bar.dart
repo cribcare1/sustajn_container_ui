3b33cb5f61a3f313d4fad32170f184e027e94344
tapanpadhi97 <tapan.padhi@ksnalabs.com>
2025-11-17 22:36:03 +0530

    add navigation

--- lib/auth/screens/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/auth/screens/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -2,21 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/number_constants.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
-    );
-  }
-}
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
