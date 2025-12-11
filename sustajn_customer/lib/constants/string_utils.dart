51a0840422c1d92e599d6a2cb0f48601cf29765d
ashwinikumarpadhi2004-cmd <padhiashwini143@gmail.com>
2025-12-11 15:02:36 +0530

    constants added

--- sustajn_customer/lib/constants/string_utils.dart
+++ sustajn_customer/lib/constants/string_utils.dart
@@ -2,4 +2,8 @@ class Strings{
 static const String LOGIN = "Log In";
 static const String SIGN_UP = "Sign Up";
 static const String FORGOT_PASSWORD = "Forgot Password";
+
+  static String get CONTAINERS => "Containers";
+  static String get ACTIVE => "Active";
+  static String get RETURNED => "Returned";
 }
