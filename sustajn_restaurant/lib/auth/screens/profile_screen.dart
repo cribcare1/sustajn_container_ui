8e8c5464c6d6a87bf56d987c9dd41ff8313f4f4c
Jogeswari197 <jogeswaripanigrahy@gmail.com>
2025-12-13 19:50:05 +0530

    screens added

--- sustajn_restaurant/lib/auth/screens/profile_screen.dart
+++ sustajn_restaurant/lib/auth/screens/profile_screen.dart
@@ -146,7 +146,7 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                     ),
                     GestureDetector(
                       onTap: (){
-                        Utility.showProfilePhotoBottomSheet(context);
+                        Utils.showProfilePhotoBottomSheet(context);
                       },
                       child: Container(
                         height: w * 0.09,
