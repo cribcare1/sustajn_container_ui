e71b2412b0e46a685cea92c6d0d45bf7ba4c366c
ravikarthi1993 <ravikarthi32@gmail.com>
2025-12-25 18:14:25 +0530

    Splash Screen Modified
    Splash Screen Modified
    image added in the assets

--- sustajn_restaurant/lib/splash_screen.dart
+++ sustajn_restaurant/lib/splash_screen.dart
@@ -1,6 +1,5 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:sustajn_restaurant/utils/app_permissons.dart';
-
 import 'auth/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,22 +9,93 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  late Animation<Offset> _logoMoveUpAnimation;
+  late Animation<double> _nameFadeAnimation;
+  late Animation<Offset> _nameSlideAnimation;
 
   @override
   void initState() {
-    AppPermissions.handleNotificationPermission();
-    AppPermissions.handleLocationPermission(context);
-    Future.delayed(Duration(seconds: 2)).then((_){
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
-    });
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    /// GIF moves slightly UP
+    _logoMoveUpAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, -0.15),
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    /// App name fades IN
+    _nameFadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    /// App name slides UP
+    _nameSlideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    /// Start animation AFTER 1 second
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) _controller.forward();
+    });
+
+    /// Navigate after splash
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            /// GIF Logo (moves up)
+            SlideTransition(
+              position: _logoMoveUpAnimation,
+              child: Image.asset(
+                'assets/images/Sustajn_logo.gif',
+                height: 200,
+                fit: BoxFit.contain,
+              ),
+            ),
+
+            /// App Name (appears after 1s)
+            FadeTransition(
+              opacity: _nameFadeAnimation,
+              child: SlideTransition(
+                position: _nameSlideAnimation,
+                child: Image.asset(
+                  'assets/images/Sustajn - logo_name.png',
+                  height: 68,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
