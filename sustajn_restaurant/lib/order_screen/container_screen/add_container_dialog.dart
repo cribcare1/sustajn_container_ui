a40de4dd1257cea02fd042ad0ab2192ef4e18df9
Romashree1999 <132276573+Romashree1999@users.noreply.github.com>
2026-01-07 00:11:53 +0530

    container qty can input manually

--- sustajn_restaurant/lib/order_screen/container_screen/add_container_dialog.dart
+++ sustajn_restaurant/lib/order_screen/container_screen/add_container_dialog.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sustajn_restaurant/utils/utility.dart';
 
 import '../../constants/number_constants.dart';
 import '../models/add_container_model.dart';
@@ -14,166 +15,244 @@ class AddContainerDialog extends StatefulWidget {
 }
 
 class _AddContainerDialogState extends State<AddContainerDialog> {
+  bool isEditingQty = false;
+  final FocusNode qtyFocusNode = FocusNode();
+  TextEditingController quantity = TextEditingController();
+
   int qty = 0;
+
+  @override
+  void dispose() {
+    quantity.dispose();
+    qtyFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return SafeArea(
-      top: false,bottom: true,
-      child: Container(
-        padding: EdgeInsets.all(Constant.CONTAINER_SIZE_16),
-        decoration: BoxDecoration(
-          color: theme.scaffoldBackgroundColor,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(Constant.CONTAINER_SIZE_30),
+      top: false,
+      bottom: true,
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(Constant.CONTAINER_SIZE_16),
+          decoration: BoxDecoration(
+            color: theme.scaffoldBackgroundColor,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(Constant.CONTAINER_SIZE_30),
+            ),
           ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: Constant.CONTAINER_SIZE_60,
-                height: Constant.SIZE_05,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                  BorderRadius.circular(Constant.SIZE_05),
-                ),
-              ),
-
-              SizedBox(height: Constant.CONTAINER_SIZE_20),
-
-              Container(
-                width: Constant.CONTAINER_SIZE_90,
-                height: Constant.CONTAINER_SIZE_90,
-                decoration: BoxDecoration(
-                  color: Constant.grey.withOpacity(0.2),
-                  borderRadius:
-                  BorderRadius.circular(Constant.CONTAINER_SIZE_20),
-                ),
-                child:
-                Image.asset(widget.item.image, fit: BoxFit.contain),
-              ),
-
-              SizedBox(height: Constant.SIZE_15),
-
-              Text(widget.item.name,
-                  style: theme.textTheme.titleLarge?.copyWith(
-                    color: Colors.white
-                  )),
-              Text(widget.item.code,
-                  style: theme.textTheme.bodySmall?.copyWith(
-                      color: Colors.white
-                  )),
-              Text(widget.item.volume,
-                  style: theme.textTheme.bodySmall?.copyWith(
-                      color: Colors.white
-                  )),
-
-              SizedBox(height: Constant.SIZE_15),
-
-              Container(
-                padding: EdgeInsets.symmetric(
-                    horizontal: Constant.CONTAINER_SIZE_16,
-                    vertical: Constant.SIZE_06),
-                decoration: BoxDecoration(
-                  borderRadius:
-                  BorderRadius.circular(Constant.CONTAINER_SIZE_20),
-                  border: Border.all(
-                      color: Constant.gold),
-                ),
-                child: Text(
-                  "Available Quantity: ${widget.item.availableQty}",
-                  style: theme.textTheme.bodySmall?.copyWith(
-                      color: Constant.gold),
-                ),
-              ),
-
-              SizedBox(height: Constant.CONTAINER_SIZE_20),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _qtyButton(
-                    icon: Icons.remove,
-                    onTap: () {
-                      if (qty > 0) {
-                        setState(() => qty--);
-                      }
-                    },
-                    theme: theme,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: Constant.CONTAINER_SIZE_60,
+                  height: Constant.SIZE_05,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(Constant.SIZE_05),
                   ),
-                  SizedBox(width: Constant.CONTAINER_SIZE_20),
-                  Text(qty.toString(),
-                      style: theme.textTheme.displaySmall?.copyWith(
-                        color: Colors.white
-                      )),
-                  SizedBox(width: Constant.CONTAINER_SIZE_20),
-                  _qtyButton(
-                    icon: Icons.add,
-                    onTap: () {
-                      if (qty < widget.item.availableQty) {
-                        setState(() => qty++);
-                      }
-                    },
-                    theme: theme,
-                  ),
-                ],
-              ),
+                ),
 
-              SizedBox(height: Constant.CONTAINER_SIZE_25),
+                SizedBox(height: Constant.CONTAINER_SIZE_20),
 
-              Row(
-                children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      onPressed: () => Navigator.pop(context),
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(color: Colors.amber, width: 1.5),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding:  EdgeInsets.symmetric(vertical: Constant.CONTAINER_SIZE_12),
-                      ),
-                      child: const Text("Cancel",
-                      style: TextStyle(color: Constant.gold),),
+                Container(
+                  width: Constant.CONTAINER_SIZE_90,
+                  height: Constant.CONTAINER_SIZE_90,
+                  decoration: BoxDecoration(
+                    color: Constant.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(
+                      Constant.CONTAINER_SIZE_20,
                     ),
                   ),
-                  SizedBox(width: Constant.CONTAINER_SIZE_12),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
+                  child: Image.asset(widget.item.image, fit: BoxFit.contain),
+                ),
+
+                SizedBox(height: Constant.SIZE_15),
+
+                Text(
+                  widget.item.name,
+                  style: theme.textTheme.titleLarge?.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  widget.item.code,
+                  style: theme.textTheme.bodySmall?.copyWith(color: Colors.white),
+                ),
+                Text(
+                  widget.item.volume,
+                  style: theme.textTheme.bodySmall?.copyWith(color: Colors.white),
+                ),
+
+                SizedBox(height: Constant.SIZE_15),
+
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Constant.CONTAINER_SIZE_16,
+                    vertical: Constant.SIZE_06,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      Constant.CONTAINER_SIZE_20,
+                    ),
+                    border: Border.all(color: Constant.gold),
+                  ),
+                  child: Text(
+                    "Available Quantity: ${widget.item.availableQty}",
+                    style: theme.textTheme.bodySmall?.copyWith(
+                      color: Constant.gold,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: Constant.CONTAINER_SIZE_20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _qtyButton(
+                      icon: Icons.remove,
+                      onTap: () {
                         if (qty > 0) {
-                          Navigator.pop(context, qty);
+                          setState(() => qty--);
                         }
                       },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Constant.gold,
-                        disabledBackgroundColor: Constant.gold,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(Constant.CONTAINER_SIZE_16),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: Constant.CONTAINER_SIZE_12,
-                        ),
-                        elevation: Constant.SIZE_00,
+                      theme: theme,
+                    ),
+                    SizedBox(width: Constant.CONTAINER_SIZE_20),
+
+                    Container(
+                      width: Constant.CONTAINER_SIZE_70,
+                      height: Constant.CONTAINER_SIZE_48,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Constant.gold, width: 1.5),
+                        borderRadius: BorderRadius.circular(Constant.CONTAINER_SIZE_12),
                       ),
-                      child: Text(
-                        "Add",
-                        style: theme.textTheme.titleMedium?.copyWith(
-                          color: theme.primaryColor,
-                          fontWeight: FontWeight.w600,
+                      child: isEditingQty
+                          ? TextField(
+                        controller: quantity,
+                        focusNode: qtyFocusNode,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        autofocus: true,
+                        style: theme.textTheme.displaySmall?.copyWith(
+                          color: Colors.white,
+                        ),
+                        cursorColor: Colors.white,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        decoration: const InputDecoration(
+                          isDense: true,
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          focusedErrorBorder: InputBorder.none,
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          contentPadding: EdgeInsets.zero,
+                        ),
+                        onSubmitted: (_) => _saveQty(),
+                        onEditingComplete: _saveQty,
+                      )
+                          : GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          setState(() {
+                            isEditingQty = true;
+                            quantity.clear();
+                          });
+                          Future.delayed(const Duration(milliseconds: 50), () {
+                            qtyFocusNode.requestFocus();
+                          });
+                        },
+                        child: Text(
+                          qty.toString(),
+                          style: theme.textTheme.displaySmall?.copyWith(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
 
-                ],
-              ),
-            ],
+                    SizedBox(width: Constant.CONTAINER_SIZE_20),
+                    _qtyButton(
+                      icon: Icons.add,
+                      onTap: () {
+                        if (qty < widget.item.availableQty) {
+                          setState(() => qty++);
+                        }
+                      },
+                      theme: theme,
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: Constant.CONTAINER_SIZE_25),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () => Navigator.pop(context),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Colors.amber, width: 1.5),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              Constant.CONTAINER_SIZE_30,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            vertical: Constant.CONTAINER_SIZE_12,
+                          ),
+                        ),
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(color: Constant.gold),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: Constant.CONTAINER_SIZE_12),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (qty > 0) {
+                            Navigator.pop(context, qty);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Constant.gold,
+                          disabledBackgroundColor: Constant.gold,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              Constant.CONTAINER_SIZE_16,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            vertical: Constant.CONTAINER_SIZE_12,
+                          ),
+                          elevation: Constant.SIZE_00,
+                        ),
+                        child: Text(
+                          "Add",
+                          style: theme.textTheme.titleMedium?.copyWith(
+                            color: theme.primaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -192,12 +271,38 @@ class _AddContainerDialogState extends State<AddContainerDialog> {
         height: Constant.CONTAINER_SIZE_40,
         decoration: BoxDecoration(
           color: Constant.white.withOpacity(0.3),
-          borderRadius:
-          BorderRadius.circular(Constant.CONTAINER_SIZE_12),
+          borderRadius: BorderRadius.circular(Constant.CONTAINER_SIZE_12),
         ),
-        child: Icon(icon,
-        color: Colors.white,),
+        child: Icon(icon, color: Colors.white),
       ),
     );
+  }
+
+  bool _toastShown = false;
+
+  void _saveQty() {
+    final int previousQty = qty;
+    final int? value = int.tryParse(quantity.text);
+
+    if (value == null || value > widget.item.availableQty) {
+      if (!_toastShown) {
+        Utils.showToast(
+          "Quantity must be between 0 and ${widget.item.availableQty}",
+        );
+        _toastShown = true;
+        Future.delayed(Duration(seconds: 2), () => _toastShown = false);
+      }
+
+      setState(() {
+        qty = previousQty;
+        isEditingQty = false;
+      });
+      return;
+    }
+
+    setState(() {
+      qty = value;
+      isEditingQty = false;
+    });
   }
 }
