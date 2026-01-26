31c303cc95b2c1e0f446154c3541ee7a65ac281f
ashwinikumarpadhi2004-cmd <padhiashwini143@gmail.com>
2026-01-26 17:28:27 +0530

    logo added

--- sustajn_restaurant/lib/constants/string_utils.dart
+++ sustajn_restaurant/lib/constants/string_utils.dart
@@ -99,6 +99,10 @@ class Strings{
   static const FILL_DETAILS = 'Please fill the below details to create your account';
   static const NAME = 'Name';
   static const MOBILE_NUMBER = 'Contact Number*';
+  static const CONTACT_PERSON = 'Contact Person';
+  static const EMAIL_REGISTRATION = 'Email Registration';
+  static const TRADE_LICENSE_NUMBER = 'Trade License Number';
+  static const VAT_NUMBER = 'VAT Number';
   static const EMAIL = 'Email ID* ';
   static const PASSWORD = 'Password*';
   static const CONFIRM_PASSWORD = 'Confirm Password*';
