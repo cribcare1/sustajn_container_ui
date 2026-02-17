14d25d3485b2ca1b408ba795439beac7a42c15e1
ashwinikumarpadhi2004-cmd <padhiashwini143@gmail.com>
2026-02-17 20:22:01 +0530

    device token added

--- sustajn_customer/lib/auth/screens/login_screen.dart
+++ sustajn_customer/lib/auth/screens/login_screen.dart
@@ -31,6 +31,20 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   final _passwordController = TextEditingController();
   bool _showPassword = false;
 
+  @override
+  void initState() {
+    Utils.getDeviceToken();
+    super.initState();
+  }
+
+  Future<void> getDevice() async {
+    final deviceToken = await Utils.getDeviceToken();
+
+    setState(() {
+
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
