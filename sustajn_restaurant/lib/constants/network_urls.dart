2b9b63cc7d99791af68eec3f94fd86c51220281f
tapanpadhi97 <tapan.padhi@ksnalabs.com>
2026-04-18 23:10:41 +0530

    issue fixed

--- sustajn_restaurant/lib/constants/network_urls.dart
+++ sustajn_restaurant/lib/constants/network_urls.dart
@@ -16,9 +16,9 @@ class NetworkUrls {
   static const REGISTER_USER_KEY = 'data';
 
   static const BASE_URL = "http://65.0.241.5:9090/";
-  static const PROFILE_IMAGE_BASE_URL = "http://35.154.182.218:9090/auth/images/profile/";
-  static const IMAGE_BASE_URL = "http://35.154.182.218:9090/auth/images/";
-  static const CONTAINER_IMAGE_BASE_URL = "http://35.154.182.218:9090/auth/images/container/";
+  static const PROFILE_IMAGE_BASE_URL = "http://65.0.241.5:9090/auth/images/profile/";
+  static const IMAGE_BASE_URL = "http://65.0.241.5:9090/auth/images/";
+  static const CONTAINER_IMAGE_BASE_URL = "http://65.0.241.5:9090/auth/images/container/";
   static const LOGIN_API = "auth/login";
   static const REGISTER_USER = "auth/register-restaurant";
   static const FORGOT_PASSWORD = "notification/forgot-password";
