d5faa32a58679d177d260df0fb4de28012ab66a0
tapanpadhi97 <tapan.padhi@ksnalabs.com>
2026-04-18 23:09:49 +0530

    issue fixed

--- sustajn_customer/lib/constants/network_urls.dart
+++ sustajn_customer/lib/constants/network_urls.dart
@@ -5,8 +5,8 @@ class NetworkUrls {
   static const int UNAUTHORIZED_ERROR_CODE = 401;
 
   static const BASE_URL = "http://65.0.241.5:9090/";
-  static const BASE_IMAGE_URL = 'http://35.154.182.218:9090/auth/images/';
-  static const PROFILE_IMAGE_BASE_URL = "http://35.154.182.218:9090/auth/images/profile/";
+  static const BASE_IMAGE_URL = 'http://65.0.241.5:9090/auth/images/';
+  static const PROFILE_IMAGE_BASE_URL = "http://65.0.241.5:9090/auth/images/profile/";
   static const LOGIN_API = "auth/login";
   static const REGISTER_USER = "auth/registerCostumer";
   static const FORGOT_PASSWORD = "notification/forgot-password";
