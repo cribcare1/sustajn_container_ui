d5eb8307cf5260c73373d7e7a6c6f0517523ab44
nayakprav614 <nayakpravin614@gmail.com>
2026-07-05 13:28:30 +0530

    Transaction Home Screen Completed.

--- sustajn_admin/lib/auth/screens/dashboard_screen.dart
+++ sustajn_admin/lib/auth/screens/dashboard_screen.dart
@@ -6,7 +6,9 @@ import 'package:container_tracking/utils/nav_utils.dart';
 import '../../Screen/users/screens/users_screen.dart';
 import '../../container_list/screens/container_list_screen.dart';
 import '../../order_request_screen/screens/order_request_home_screen.dart';
+import '../../transactions/screens/transaction_home_screen.dart';
 import '../../utils/theme_utils.dart';
+import 'package:container_tracking/Screen/users/screens/transaction_home_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -181,6 +183,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
     case "Users": NavUtil.navigateToPushScreen(context, UsersScreen());
     break;
     case "Order Requests": NavUtil.navigateToPushScreen(context, OrderRequestScreen());
+    break;
+    case "Transactions":
+      NavUtil.navigateToPushScreen(
+        context,
+        TransactionHomeScreen(
+          userId: 1,
+        ),
+      );
+      break;
     default:
       break;
   }
