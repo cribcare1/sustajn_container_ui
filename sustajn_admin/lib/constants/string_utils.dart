9bbe024c20070c6f5e2da782e5a33f2dfafe31c7
nayakprav614 <nayakpravin614@gmail.com>
2026-07-25 12:24:49 +0530

    Damage Screen Completed.

--- sustajn_admin/lib/constants/string_utils.dart
+++ sustajn_admin/lib/constants/string_utils.dart
@@ -312,15 +312,6 @@ class Strings {
   static const String PAY_USE = 'Pay-per-use';
   static const String INCRICULATION = 'In Circulation';
   static const String SEARCH_BY_USERID = 'Search by UserID';
-  //Transactions
-  // static const String TRANSACTIONS = 'Transactions';
-  // static const String NO_SOLD_CON = 'No sold containers found';
-  // static const String NO_SUBSCRIPTION  = 'No Subscription Plan found';
-  // static const String NO_TRANSACTION_EXTENDED_FEE_DATA = 'No extended fee data found';
-  // static const SEARCH_BY_REST = 'Search by Partner';
-  // static const SEARCH_BY_SOLD = 'Search by Customer or Partner';
-  // static const SEARCH_BY_EXTENDED_FEE = 'Search by Customer ID';
-  // static const PLANTYPE = 'Plan Type';
 
  static const String SUBSCRIPTION = 'Subscription';
  static const String EXTENDED_FEE = 'Extended Fee';
